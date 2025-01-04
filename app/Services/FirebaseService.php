cf8f98031a0aadfb1ee42f11507a07c1681a25c6
mastermoead@gmail.com <mastermoead@gmail.com>
2025-01-04 12:36:24 +0300

    .

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -13,26 +13,26 @@ class FirebaseService
     protected $messaging;
     public function __construct()
     {
-        $serviceAccountPath = storage_path('app/medicationswarehouse-firebase-adminsdk-ink.json');
-        $factory = (new Factory)->withServiceAccount($serviceAccountPath);
-        $this->messaging = $factory->createMessaging();
+        // $serviceAccountPath = storage_path('app/medicationswarehouse-firebase-adminsdk-ink.json');
+        // $factory = (new Factory)->withServiceAccount($serviceAccountPath);
+        // $this->messaging = $factory->createMessaging();
     }
 
    
     public function sendNotification($title, $body, $token, $data = [])
     {
-        $message = CloudMessage::withTarget('token', $token)
-            ->withNotification(['title' => $title, 'body' => $body])
-            ->withData($data);
+        // $message = CloudMessage::withTarget('token', $token)
+        //     ->withNotification(['title' => $title, 'body' => $body])
+        //     ->withData($data);
 
-        try {
-            $this->messaging->send($message);
-        } catch (\Kreait\Firebase\Exception\Messaging\InvalidMessage $e) {
-            Log::warning("Invalid FCM token : $token - Error: " . $e->getMessage());
-            return false;
-        } catch (\Exception $e) {
-            Log::error(" Error in seending the notification  FCM token: $token - Error: " . $e->getMessage());
-            return false;
-        }
+        // try {
+        //     $this->messaging->send($message);
+        // } catch (\Kreait\Firebase\Exception\Messaging\InvalidMessage $e) {
+        //     Log::warning("Invalid FCM token : $token - Error: " . $e->getMessage());
+        //     return false;
+        // } catch (\Exception $e) {
+        //     Log::error(" Error in seending the notification  FCM token: $token - Error: " . $e->getMessage());
+        //     return false;
+        // }
     }
 }
